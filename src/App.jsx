f69c5d9c659e20dbb3f8d6ff384d180a599c7fa2
Aadarsh <aadarsh0811@gmail.com>
2026-09-20 21:08:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-amber-200 px-10 py-12">
+      <h1 className="text-red-900 font-bold text-2xl">
+        Hello
+      </h1>
+    </div>
+  )
+}
+
+export default App
